f69a64d93940930d162fdfb5c73ab8ecb5354ad8
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-09-11 13:32:05 +0300

    Handle Errors in your API

---
+++ ShoppingListApi/Controllers/ErrorsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ShoppingListApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {
+   
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if(!hostEnvironment.IsDevelopment())
+            {
+                return NotFound();
+            }
+
+            var exeptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(detail: exeptionHandlerFeature.Error.StackTrace,
+                 title: exeptionHandlerFeature.Error.Message);
+        }
+
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            return Problem();   
+        }
+
+        [HttpGet("Throw")]
+        public IActionResult Throw()
+        {
+            throw new Exception("Sample Exception");
+        }
+
+    }
+}
